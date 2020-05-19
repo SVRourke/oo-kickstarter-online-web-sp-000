c4bb2621e5eecf8f7bea557dea3c60ed49d31ee2
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-19 00:48:10 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+class Project
+  attr_reader :backers, :title
+  def initialize(title)
+    @title = title
+    @backers = Array.new
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self)
+  end
+end
